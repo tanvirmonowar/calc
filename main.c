be81ae05282c4851a4aefa4cad106248bd2fdc45
tanvirmonowar <tanvirmonowar5@gmail.com>
2026-03-02 23:51:30 +0600

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main()
 {
 	float x,y;
 	char ch;
-	printf("Enter x,y & symbol(+-*/):");
+	printf("Enter x,y & symbol (this is changed)(+-*/):");
 	scanf("%f %f %c",&x,&y,&ch);
 	op(x,y,ch);
 	return 0;
